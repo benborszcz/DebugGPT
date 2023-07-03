13f0330012dc3324891b75e1e50c85e7089c31b4
Ben Borszcz <ben.borszcz@gmail.com>
2023-07-03 14:14:24 -0400

    tenacity->backoff

--- agent.py
+++ agent.py
@@ -1,7 +1,7 @@
 import config
 import openai
 import json
-from tenacity import retry, wait_random_exponential, stop_after_attempt
+import backoff
 
 # Set the API Params for OpenAI
 openai.api_key = config.OPENAI_API_KEY
@@ -26,7 +26,44 @@ class Agent:
         response = self.chat_completion(local_messages)
         return response
     
-    @retry(wait=wait_random_exponential(min=1, max=40), stop=stop_after_attempt(3))
+    @backoff.on_exception(backoff.expo, Exception, max_tries=3)
+    def chat_completion(self, messages: list, temperature=0.4, presence_penalty=0.1, frequency_penalty=0.1, max_tokens=1000):
+        # Generate a response using the OpenAI API
+        if self.functions == None:
+            response = openai.ChatCompletion.create(
+                model="gpt-3.5-turbo-0613",
+                messages=messages,
+                temperature=temperature,
+                presence_penalty=presence_penalty,
+                frequency_penalty=frequency_penalty,
+                max_tokens=max_tokens,
+            )
+            return response
+        else:
+            passed_functions = []
+            function_response = None
+            for key, value in self.functions.items():
+                passed_functions.append(value.to_dict())
+
+            response = openai.ChatCompletion.create(
+                model="gpt-3.5-turbo-0613",
+                messages=messages,
+                temperature=temperature,
+                presence_penalty=presence_penalty,
+                frequency_penalty=frequency_penalty,
+                max_tokens=max_tokens,
+                functions=passed_functions
+            )
+            response_message = response["choices"][0]["message"]
+            if response_message.get("function_call"):
+                # Step 3: call the function
+                # Note: the JSON response may not always be valid; be sure to handle errors
+                function_name = response_message["function_call"]["name"]
+                fuction_to_call = self.functions[function_name]
+                function_args = json.loads(response_message["function_call"]["arguments"])
+                function_response = fuction_to_call.process_instruction(function_args)
+                return {'choices': [{'message': {'content':function_response}}]}
+            return response
     def chat_completion(self, messages: list, temperature=0.4, presence_penalty=0.1, frequency_penalty=0.1, max_tokens=1000):
         # Generate a response using the OpenAI API
         if self.functions == None:
--- test_script.py
+++ test_script.py
@@ -1,5 +1,7 @@
 print("geek")
 
+j = 0  # Define 'j' with an initial value
+
 for i in range(3):
     a = j * i
 
