f30344f3e918f6e9316aeca4686748bd53810f9b
Ben Borszcz <ben.borszcz@gmail.com>
2023-07-04 00:32:36 -0400

    added verbose

--- dgpt/cli.py
+++ dgpt/cli.py
@@ -4,6 +4,7 @@ from .agent_manager import AgentManager
 from .change_logger import ChangeLogger
 from rich.console import Console
 from rich.traceback import install
+import argparse
 import inquirer
 import sys
 import pathspec
@@ -69,35 +70,35 @@ def extract_filenames(text):
     return output
 
 # Define a function to debug a script
-def debug_script(script):
+def debug_script(script, verbose):
     # Create a change logger object
     change_logger = ChangeLogger(os.path.dirname(os.path.abspath(script)))
     # Create a script runner object
     runner = ScriptRunner(script)
-    console.print(f"-----Running {script}-----", style="bold blue")
+    console.print(f"Running {script}", style="bold blue")
     # Run the script and get the status and output
     status, output = runner.run_script()
     console.print(output)
     # If the script run status is not successful
     if not status:
-        console.print("-----Analyzing Output-----", style="bold blue")
+        console.print("Analyzing Output", style="bold blue")
         # Generate error analysis output
         error_analysis_output = manager.generate("ErrorAnalysis", [{"role":"user","content":output}])
-        console.print(error_analysis_output, style="bold red")
-        console.print("-----Requesting Files-----", style="bold blue")
+        if verbose: console.print(error_analysis_output, style="bold red")
+        console.print("Requesting Files", style="bold blue")
         # Generate file getter output
         file_getter_output = manager.generate("FileRequester", [{"role":"user","content":str(error_analysis_output)+"\n\n"+str(visualize_file_structure(os.getcwd()))}])
-        console.print(extract_filenames(file_getter_output), style="bold green")
-        console.print("-----Planning Solution-----", style="bold blue")
+        if verbose: console.print(extract_filenames(file_getter_output), style="bold green")
+        console.print("Planning Solution", style="bold blue")
         # Generate step planner output
         step_planner_output = manager.generate("StepPlanner", [{"role":"user","content":(str(error_analysis_output)+"\n\n"+str(file_getter_output))}])
-        console.print(step_planner_output, style="bold green")
-        console.print("-----Editing Files-----", style="bold blue")
+        if verbose: console.print(step_planner_output, style="bold green")
+        console.print("Editing Files", style="bold blue")
         # Generate code modifier output
         code_modifier_output = manager.generate("CodeModifier", [{"role":"user","content":(str(error_analysis_output)+"\n\n"+str(file_getter_output)+"\n\n"+str(step_planner_output))}])
-        console.print(code_modifier_output, style="bold green")
+        if verbose: console.print(code_modifier_output, style="bold green")
         change_logger.check_changes()
-        console.print(f"-----Running {script}-----", style="bold blue")
+        console.print(f"Running {script}", style="bold blue")
         # Run the script again and get the new status and output
         runner = ScriptRunner(script)
         status, new_output = runner.run_script()
@@ -107,24 +108,24 @@ def debug_script(script):
 
         # If the new script run status is not successful
         if not status:
-            console.print("DebugGPT has identified another/new error", style="bold red")
-            console.print("-----Analyzing Output-----", style="bold blue")
+            console.print("DebugGPT has identified another/new error\n", style="bold red")
+            console.print("Analyzing Output", style="bold blue")
             # Generate edit analysis output
             edit_analysis_output = manager.generate("ErrorAnalysis", [{"role":"user","content":new_output}])
-            console.print(edit_analysis_output, style="bold red")
-            console.print("-----Analyzing Progress-----", style="bold blue")
+            if verbose: console.print(edit_analysis_output, style="bold red")
+            console.print("Analyzing Progress", style="bold blue")
             # Generate compare errors output
             compare_errors_output = manager.generate("ErrorComparison", [{"role":"user","content":f"Old Error: {str(error_analysis_output)}\n\nNew Error:{str(edit_analysis_output)}"}])
-            console.print(compare_errors_output, style="bold green")
+            if verbose: console.print(compare_errors_output, style="bold green")
             # Generate progress id output
             progress_id_output = manager.generate("ProgressIdentifier", [{"role":"user","content":str(compare_errors_output)}])
             # If progress was made
             if progress_id_output == 't': 
-                console.print("%%DebugGPT has identified progress was made", style="bold green")
+                console.print("\nDebugGPT has identified progress was made\n", style="bold green")
             else: 
-                console.print("%%DebugGPT has identified no/reverse progress was made", style="bold red")
+                console.print("\nDebugGPT has identified no/reverse progress was made\n", style="bold red")
         
-        console.print("-----Deciding Next Step-----", style="bold blue")
+        console.print("Deciding Next Step", style="bold blue")
         console.print("***User Input Required***", style="bold yellow")
         # Display the changes in the script
         change_logger.display_changes()
@@ -138,15 +139,15 @@ def debug_script(script):
         answers = inquirer.prompt(questions)
         # If the user chooses to continue
         if answers['choice'] == 'Continue':
-            console.print("-----Continuing Debug-----", style="bold blue")
+            console.print("Continuing Debug", style="bold blue")
             # Debug the script again
             debug_script(script)
         # If the user chooses to revert
         elif answers['choice'] == 'Revert':
-            console.print("-----Reverting Changes-----", style="bold blue")
+            console.print("Reverting Changes", style="bold blue")
             # Revert the last change in the script
             change_logger.revert_last_change()
-            console.print("-----Deciding Next Step-----", style="bold blue")
+            console.print("Deciding Next Step", style="bold blue")
             console.print("***User Input Required***", style="bold yellow")
             questions = [
                 inquirer.List('choice',
@@ -158,23 +159,20 @@ def debug_script(script):
             answers = inquirer.prompt(questions)
             # If the user chooses not to debug again
             if answers['choice'] == 'No': return False
-            console.print("-----Continuing Debug-----", style="bold blue")
+            console.print("Continuing Debug", style="bold blue")
             # Debug the script again
-            debug_script(script)
+            debug_script(script, verbose)
     return status
 
 # Define the main function
 def main():
-    # If the number of command line arguments is not 2
-    if len(sys.argv) != 2:
-        print("Usage: dgpt file_name.py")
-        # Exit the script
-        sys.exit(1)
+    # Create an argument parser
+    parser = argparse.ArgumentParser(description='Debug a Python script with DebugGPT.')
+    parser.add_argument('script_file', type=str, help='The Python script to debug.')
+    parser.add_argument('-v', '--verbose', action='store_true', help='Enable verbose output.')
 
-    # Get the script file from the command line arguments
-    script_file = sys.argv[1]
-    console.rule("[bold blue]DebugGPT")
-    # Debug the script file
-    debug_script(script_file)
-    console.print("All Errors Solved or DebugGPT Terminated", style="italic purple")
-    console.rule("[bold blue]https://github.com/benborszcz/DebugGPT")
+    # Parse the command line arguments
+    args = parser.parse_args()
+
+    # Pass the verbose flag to the debug_script function
+    debug_script(args.script_file, args.verbose)
